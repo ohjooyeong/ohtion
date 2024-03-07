4d924d70a2afb273ecb488aa41e5c8b621c07aa3
OH-JOO-YEONG <brb1111@naver.com>
2024-03-07 23:23:24 +0900

    [Feat] convex 설정, clerk 설정 authenticated 연동, 로딩 컴포넌트 구현

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,9 +1,15 @@
 "use client";
 
+import { Spinner } from "@/components/spinner";
 import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -14,9 +20,26 @@ const Heading = () => {
         Ohtion is the connected workspace where <br /> better, faster work
         happens.
       </h3>
-      <Button>
-        Enter Ohtion <ArrowRight className="h-4 w-4 ml-2" />
-      </Button>
+      {isLoading && (
+        <div className="w-full flex items-center justify-center">
+          <Spinner size={"lg"} />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href={"/documents"}>
+            Enter Ohtion <ArrowRight className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button>
+            Get Ohtion free
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Button>
+        </SignInButton>
+      )}
     </div>
   );
 };
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://charmed-bonefish-33.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};

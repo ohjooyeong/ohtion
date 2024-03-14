29f35d84d92448127697f1c709c5d0821ec90503
OH-JOO-YEONG <brb1111@naver.com>
2024-03-14 23:00:52 +0900

    [Feat] Document 상세 페이지 Navbar 작업
    - document title 수정 기능

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -10,7 +10,7 @@ import {
   Settings,
   Trash,
 } from 'lucide-react';
-import { usePathname } from 'next/navigation';
+import { useParams, usePathname } from 'next/navigation';
 import {
   ElementRef,
   useEffect,
@@ -33,10 +33,12 @@ import {
 import { TrashBox } from './trash-box';
 import { useSearch } from '@/hooks/use-search';
 import { useSettings } from '@/hooks/use-settings';
+import { Navbar } from './navbar';
 
 export const Navigation = () => {
   const settings = useSettings();
   const search = useSearch();
+  const params = useParams();
   const pathname = usePathname();
   const isMobile = useMediaQuery('(max-width: 768px)');
 
@@ -226,15 +228,22 @@ export const Navigation = () => {
           isMobile && 'left-0 w-full',
         )}
       >
-        <nav className="bg-transparent px-3 py-2 w-full">
-          {isCollapsed && (
-            <MenuIcon
-              onClick={resetWidth}
-              role="button"
-              className="h-6 w-6 text-muted-foreground"
-            />
-          )}
-        </nav>
+        {!!params.documentId ? (
+          <Navbar
+            isCollapsed={isCollapsed}
+            onResetWidth={resetWidth}
+          />
+        ) : (
+          <nav className="bg-transparent px-3 py-2 w-full">
+            {isCollapsed && (
+              <MenuIcon
+                onClick={resetWidth}
+                role="button"
+                className="h-6 w-6 text-muted-foreground"
+              />
+            )}
+          </nav>
+        )}
       </div>
     </>
   );
